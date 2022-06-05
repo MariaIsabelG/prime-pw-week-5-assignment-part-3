ac314f594b9cf555e305febf4168990e37147c8a
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-05 13:35:31 -0500

    cleaned up instructions

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,7 +17,7 @@ let collection=[];
 //- Add a function named `addToCollection`. This function should:
 //  - Take in the album's `title`, `artist`, `yearPublished` as input parameters
 //  - Create a new object having the above properties
-// - Add the new object to the end of the `collection` array
+//  - Add the new object to the end of the `collection` array
 //  - Return the newly created object
 
 //- Test the `addToCollection` function:
