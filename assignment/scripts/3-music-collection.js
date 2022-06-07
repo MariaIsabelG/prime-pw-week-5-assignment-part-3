d87b7daf4e8b772967b4c8272a4725c85d42c337
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-06 19:53:42 -0500

    cleaned up code

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,16 +7,16 @@ function addToCollection ( title, artist, yearPublished ){
         newObject.title = title;
         newObject.artist = artist;
         newObject.yearPublished = yearPublished; 
-    collection.push(newObject)
-    return console.log(newObject);
+    collection.push( newObject )
+    return console.log( newObject );
 } //end of function to create new objects with properties (title, artist, yearPublished) and add objects to collection array
 
-addToCollection('Rio', 'Perota Chingo', 2020 );
+addToCollection( 'Rio', 'Perota Chingo', 2020 );
 addToCollection( 'Amor eterno', 'Juan Gabriel', 2020 );
 addToCollection( 'Abrazame', 'Juan Gabriel', 1990 ); 
-addToCollection('Melones', 'Perota Chingo', 1990);
-addToCollection('Melones', 'Lizzo', 2019);
-addToCollection( 'Mañana', 'Juanes', 2006); 
+addToCollection( 'Melones', 'Perota Chingo', 1990 );
+addToCollection( 'Melones', 'Lizzo', 2019 );
+addToCollection( 'Mañana', 'Juanes', 2006 ); 
 // testing function and logging all new objects
 
 console.log( 'The collection array is now:', collection );// logging the updated collection array
@@ -24,14 +24,14 @@ console.log( 'The collection array is now:', collection );// logging the updated
 
 function showCollection ( anyArray ){
     console.log( 'The number of this array is:', anyArray.length );
-    for( let i=0; i<anyArray.length; i++){
-        console.log(`${anyArray[i].title} by ${anyArray[i].artist}, published in ${anyArray[i].yearPublished}`)
+    for( let i=0; i<anyArray.length; i++ ){
+        console.log( `${anyArray[i].title} by ${anyArray[i].artist}, published in ${anyArray[i].yearPublished}` )
     }return anyArray
     }//function to log each object in the collection array
 
 showCollection(collection);// testing showCollection function
 
-function findByArtist (artist){
+function findByArtist ( artist ){
     let newArray=[];
     for( i=0; i<collection.length; i++ ){
     if( collection[i].artist === artist ){
@@ -40,6 +40,6 @@ function findByArtist (artist){
     }return console.log( newArray )
 }// function searching by artist
 
-findByArtist('Lizzo');
-findByArtist('Juan Gabriel');
-findByArtist('Shakira');//testing different artists
+findByArtist( 'Lizzo' );
+findByArtist( 'Juan Gabriel' );
+findByArtist( 'Shakira' );//testing different artists
